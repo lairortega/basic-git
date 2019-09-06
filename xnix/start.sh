bd88a643e6195abe5c9ed852a3594fabcbb9f24e
Lair Ortega Dehonor <lair.ortega@gmail.com>
2019-09-06 15:29:01 -0500

    escenario 5

--- xnix/start.sh
+++ xnix/start.sh
@@ -2,6 +2,5 @@
 cd ..
 echo "<h1>Index del sitio</h1>" > index.html
 git add -A
-git commit -m "crea y agrega archivo index al repositorio."
-echo "<p>Modificamos el archivo index para generarle un cambio</p>" >> index.html
-echo "<p>y poder deshacerlo con git.</p>" >> ../index.html
+git commit -m "Agregar el archivo site.css a este commit."
+touch site.css
