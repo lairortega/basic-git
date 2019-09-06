090dfb5033dea4e5be210e4bca03352bedaf528b
Lair Ortega Dehonor <lair.ortega@gmail.com>
2019-09-06 14:03:35 -0500

    crea y agrega archivo index al repositorio.

--- xnix/start.sh
+++ xnix/start.sh
@@ -1,6 +1,6 @@
 #!/binbash
 echo "<h1>Index del sitio</h1>" > ../index.html
-git add ../index.html
+git add -A
 git commit -m "crea y agrega archivo index al repositorio."
-echo "<p>Modificamos el archivo index para generarle un cambio</p>"
-echo "<p>y poder deshacerlo con git.</p>"
+echo "<p>Modificamos el archivo index para generarle un cambio</p>" >> ../index.html
+echo "<p>y poder deshacerlo con git.</p>" >> ../index.html
