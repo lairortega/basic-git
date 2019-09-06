91f7975f59eca574710b7b2e2797cccbf065c66d
Lair Ortega Dehonor <lair.ortega@gmail.com>
2019-09-06 15:03:19 -0500

    escenario 3

--- xnix/start.sh
+++ xnix/start.sh
@@ -1,6 +1,8 @@
 #!/binbash
+cd ..
 echo "<h1>Index del sitio</h1>" > ../index.html
 git add -A
-git commit -m "crea y agrega archivo index al repositorio."
-echo "<p>Modificamos el archivo index para generarle un cambio</p>" >> ../index.html
-echo "<p>y poder deshacerlo con git.</p>" >> ../index.html
+git commit -m "MODIFICAR ESTE MENSAJE."
+echo "<p>Condenido adicional</p>" > ../index.html
+git add -A
+git commit -m "Mensaje de un commit que está correcto."
