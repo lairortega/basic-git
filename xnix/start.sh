6560c79ba73030c2af7caf804bcd1976a14efb48
Lair Ortega Dehonor <lair.ortega@gmail.com>
2019-09-06 15:06:20 -0500

    escenario 3

--- xnix/start.sh
+++ xnix/start.sh
@@ -1,8 +1,8 @@
 #!/binbash
 cd ..
-echo "<h1>Index del sitio</h1>" > ../index.html
+echo "<h1>Index del sitio</h1>" > index.html
 git add -A
 git commit -m "MODIFICAR ESTE MENSAJE."
-echo "<p>Condenido adicional</p>" > ../index.html
+echo "<p>Condenido adicional</p>" >> index.html
 git add -A
 git commit -m "Mensaje de un commit que está correcto."
