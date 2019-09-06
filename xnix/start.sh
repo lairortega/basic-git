098550147b5e8073a831647846406c344c5d131e
Lair Ortega Dehonor <lair.ortega@gmail.com>
2019-09-06 16:25:05 -0500

    escenario 5

--- xnix/start.sh
+++ xnix/start.sh
@@ -3,8 +3,9 @@ cd ..
 git checkout -b tempotal-branch
 echo "<h1>Index del sitio</h1>" > index.html
 git add -A
-git commit -m "Agregar el archivo site.css a este commit."
+git commit -m "Commit en histórico correcto, (Algún punto en producción)"
 echo "<p>Contenido del archivo</p>" >> index.html
 touch site.css
-git commit -m "esto podría ser un hotfix"
+git add -A
+git commit -m "Ésto podría ser un hotfix en producción"
 git checkout 005-move-commit-to-another-branch
