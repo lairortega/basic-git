57a95e3148a9c4b7f8d13ba67d8f25859e21df4b
Lair Ortega Dehonor <lair.ortega@gmail.com>
2019-09-06 15:14:37 -0500

    master

--- xnix/start.sh
+++ xnix/start.sh
@@ -1,6 +1,7 @@
 #!/binbash
-echo "<h1>Index del sitio</h1>" > ../index.html
+cd ..
+echo "<h1>Index del sitio</h1>" > index.html
 git add -A
 git commit -m "crea y agrega archivo index al repositorio."
-echo "<p>Modificamos el archivo index para generarle un cambio</p>" >> ../index.html
+echo "<p>Modificamos el archivo index para generarle un cambio</p>" >> index.html
 echo "<p>y poder deshacerlo con git.</p>" >> ../index.html
