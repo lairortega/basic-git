a283a7ec2f5a649ba11d56eaea912eb457b9f5a1
Lair Ortega Dehonor <lair.ortega@gmail.com>
2019-09-10 10:58:46 -0500

    escenario 7

--- xnix/start.sh
+++ xnix/start.sh
@@ -1,4 +1,19 @@
 #!/binbash
 cd ..
 mkdir {css,js,img}
-touch {css/{site.css,app.css,mobile.css},img/{log.png,bg.jpe},js/{login.js,contact.js}}
+touch {css/{site.css,app.css,mobile.css},img/{log.png,bg.jpg},js/{login.js,contact.js}}
+
+git add css/site.css
+git commit -m "site.css"
+git add img/logo.png
+git commit -m "logo.png"
+git add js/login.js
+git commit -m "login.js"
+git add css/app.css
+git commit -m "app.css"
+git add img/bg.jpg
+git commit -m "bg.jpg"
+git add js/contact.js
+git commit -m "contact.js"
+git add css/mobile.css
+git commit -m "mobile.css"
