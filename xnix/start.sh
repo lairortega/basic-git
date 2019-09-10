0c2154c567882c873e062d0c22b9a2f6d9e9fa7e
Lair Ortega Dehonor <lair.ortega@gmail.com>
2019-09-10 11:26:04 -0500

    escenario 9

--- xnix/start.sh
+++ xnix/start.sh
@@ -3,15 +3,5 @@ cd ..
 mkdir {css,js,img}
 touch {css/{login.css,contact.css},img/{login.png,contact.jpg},js/{login.js,contact.js}}
 
-git add css/login.css
-git commit -m "login.css"
-git add img/login.png
-git commit -m "login.png"
-git add js/login.js
-git commit -m "login.js"
-git add css/contact.css
-git commit -m "contact.css"
-git add img/contact.jpg
-git commit -m "contact.jpg"
-git add js/contact.js
-git commit -m "contact.js"
+git add -A
+git commit -m "update"
