9cf9efd23fd100742f00e080abbfc71ce639cdb1
Lair Ortega Dehonor <lair.ortega@gmail.com>
2019-09-10 10:48:54 -0500

    escenario 6

--- xnix/start.sh
+++ xnix/start.sh
@@ -1,7 +1,4 @@
 #!/binbash
 cd ..
-echo "<h1>Index del sitio</h1>" > index.html
-git add -A
-git commit -m "crea y agrega archivo index al repositorio."
-echo "<p>Modificamos el archivo index para generarle un cambio</p>" >> index.html
-echo "<p>y poder deshacerlo con git.</p>" >> ../index.html
+mkdir {css,js,img}
+touch {css/{site.css,app.css,mobile.css},img/{log.png,bg.jpe},js/{login.js,contact.js}}
