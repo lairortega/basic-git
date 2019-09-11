8c99aa03cfac34db27bd2b830d62a4836eaeaf2a
Lair Ortega Dehonor <lair.ortega@gmail.com>
2019-09-11 18:04:28 -0500

    escenario 5

--- xnix/start.sh
+++ xnix/start.sh
@@ -1,11 +1,18 @@
 #!/binbash
 cd ..
-git checkout -b tempotal-branch
-echo "<h1>Index del sitio</h1>" > index.html
+
+git checkout master
+echo "<h1>Inicio del desarrollo</h1>" > index.html
 git add -A
-git commit -m "Commit en histórico correcto, (Algún punto en producción)"
+git commit -m "Commit en histórico correcto, (Algún punto en desarrollo)."
+
+git checkout -b release
 echo "<p>Contenido del archivo</p>" >> index.html
 touch site.css
 git add -A
-git commit -m "Ésto podría ser un hotfix en producción"
+git commit -m "Ésto podría ser un hotfix en producción."
+
 git checkout master
+echo "<p>Continuamos desarrollando</p>" > index.html
+git add -A
+git commit -m "mensaje de desarrollo."
